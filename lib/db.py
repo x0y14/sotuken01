9d026ed85c3d40e936f623a2690c406cf86e1bee
x0y14 <ya01ma08@gmail.com>
2024-04-16 19:27:49 +0900

    get_tracks/albumsを追加

--- lib/db.py
+++ lib/db.py
@@ -1,5 +1,6 @@
 import sqlite3
 from dataclasses import dataclass
+from typing import List
 
 
 @dataclass
@@ -123,3 +124,58 @@ def is_album_exists(conn: sqlite3.Connection, album_id: str) -> bool:
     cursor = conn.cursor()
     cursor.execute("select * from albums where id=?", (album_id,))
     return cursor.fetchone() is not None
+
+
+def get_album_list(conn: sqlite3.Connection, limit: int, offset: int) -> List[Album]:
+    cursor = conn.cursor()
+    cursor.execute("""select * from albums limit ? offset ?;""", (limit, offset,))
+    album_datas = cursor.fetchall()
+
+    album_list = []
+    album_cols = ["id", "name"]
+    for album_data_only in album_datas:
+        album_data = dict(zip(album_cols, album_data_only))
+        album_list.append(Album(id=album_data["id"], name=album_data["name"]))
+
+    return album_list
+
+
+def get_tracks(conn: sqlite3.Connection, album_id: str, limit: int, offset: int) -> List[Track]:
+    cursor = conn.cursor()
+    cursor.execute(
+        """select * from tracks where album_id=? limit ? offset ?;""",
+        (album_id, limit, offset,)
+    )
+    track_datas = cursor.fetchall()
+
+    track_list = []
+    track_cols = [
+        "id", "name", "album_id", "acousticness",
+        "danceability", "duration_ms", "energy",
+        "instrumentalness", "key", "liveness",
+        "loudness", "mode", "speechiness",
+        "tempo", "time_signature", "valence"]
+    for track_data_only in track_datas:
+        track_data = dict(zip(track_cols, track_data_only))
+        track_list.append(
+            Track(
+                id=track_data["id"],
+                name=track_data["name"],
+                album_id=track_data["album_id"],
+                acousticness=track_data["acousticness"],
+                danceability=track_data["danceability"],
+                duration_ms=track_data["duration_ms"],
+                energy=track_data["energy"],
+                instrumentalness=track_data["instrumentalness"],
+                key=track_data["key"],
+                liveness=track_data["liveness"],
+                loudness=track_data["loudness"],
+                mode=track_data["mode"],
+                speechiness=track_data["speechiness"],
+                tempo=track_data["tempo"],
+                time_signature=track_data["time_signature"],
+                valence=track_data["valence"],
+            )
+        )
+
+    return track_list
